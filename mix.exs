48631c4ae1c44f3cb854d8866062a4df8a2d470c
Greg Mefford <greg@gregmefford.com>
2015-11-22 21:37:28 -0500

    Fixed the problem by actually including the ALE dependency

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Neopixel.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
+    [applications: [:logger, :elixir_ale],
      mod: {Neopixel, []}]
      #mod: {Elixir.IEx, []}]
   end
