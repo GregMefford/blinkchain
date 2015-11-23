171a5d5fcd0e5c2bda33350ec8c6404781174612
Greg Mefford <greg@gregmefford.com>
2015-11-22 21:41:00 -0500

    Simplify the code a little

--- lib/neopixel.ex
+++ lib/neopixel.ex
@@ -1,29 +1,24 @@
 defmodule Blinker do
+  require Gpio
   use GenServer
 
   def start_link(pin, opts) do
     {:ok, pid} = Gpio.start_link(pin, :output)
     GenServer.start_link(__MODULE__, pid, opts)
-    blink_forever(pid)
   end
 
   def handle_call(:on,  _from, pid), do: {:reply, Gpio.write(pid, 1)}
   def handle_call(:off, _from, pid), do: {:reply, Gpio.write(pid, 0)}
 
   def blink_forever(pid) do
-    # Turn on the green LED and sleep for 1000ms
-    #Logger.debug "Turning ON green"
     Gpio.write(pid, 1)
 
     :timer.sleep 1000
 
-    # Turn off the green LED and sleep for 1000ms
-    #Logger.debug "Turning OFF green"
     Gpio.write(pid, 0)
 
     :timer.sleep 1000
 
-    # Blink again
     blink_forever(pid)
   end
 
@@ -47,7 +42,6 @@ end
 defmodule Neopixel do
   use Application
   require Logger
-  require Gpio
 
   def start(_type, _args) do
     Neopixel.Supervisor.start_link
