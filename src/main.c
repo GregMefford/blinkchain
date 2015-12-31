4196f85040b5b1e08438ff8943eddc448b071eb7
Greg Mefford <greg@gregmefford.com>
2015-12-31 15:40:00 -0500

    Prep the rpi_ws281x driver to interface with Elixir.

--- src/main.c
+++ src/main.c
@@ -1,43 +1,7 @@
-/*
- * main.c
- *
- * Copyright (c) 2014 Jeremy Garff <jer @ jers.net>
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met:
- *
- *     1.  Redistributions of source code must retain the above copyright notice, this list of
- *         conditions and the following disclaimer.
- *     2.  Redistributions in binary form must reproduce the above copyright notice, this list
- *         of conditions and the following disclaimer in the documentation and/or other materials
- *         provided with the distribution.
- *     3.  Neither the name of the owner nor the names of its contributors may be used to endorse
- *         or promote products derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <signal.h>
+#include <errno.h>
 
 #include "clk.h"
 #include "gpio.h"
@@ -46,143 +10,61 @@
 
 #include "ws2811.h"
 
+#define DMA_CHANNEL 5
 
-#define ARRAY_SIZE(stuff)                        (sizeof(stuff) / sizeof(stuff[0]))
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <GPIO Pin> <LED Count>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
-#define TARGET_FREQ                              WS2811_TARGET_FREQ
-#define GPIO_PIN                                 18
-#define DMA                                      5
+  uint8_t pin = atoi(argv[1]);
+  uint32_t count = strtol(argv[2], NULL, 10);
 
-#define WIDTH                                    8
-#define HEIGHT                                   8
-#define LED_COUNT                                (WIDTH * HEIGHT)
-
-
-ws2811_t ledstring =
-{
-    .freq = TARGET_FREQ,
-    .dmanum = DMA,
-    .channel =
-    {
-        [0] =
-        {
-            .gpionum = GPIO_PIN,
-            .count = LED_COUNT,
-            .invert = 0,
-            .brightness = 255,
-        },
-        [1] =
-        {
-            .gpionum = 0,
-            .count = 0,
-            .invert = 0,
-            .brightness = 0,
-        },
+  ws2811_t ledstring = {
+    .freq = WS2811_TARGET_FREQ,
+    .dmanum = DMA_CHANNEL,
+    .channel = {
+      [0] = {
+        .gpionum = pin,
+        .count = count,
+        .invert = 0,
+        .brightness = 255,
+      },
+      [1] = {
+        .gpionum = 0,
+        .count = 0,
+        .invert = 0,
+        .brightness = 0,
+      },
     },
-};
+  };
 
-ws2811_led_t matrix[WIDTH][HEIGHT];
+  if (ws2811_init(&ledstring)) {
+    exit(EXIT_FAILURE);
+  }
 
+  uint8_t counter = 0;
 
-void matrix_render(void)
-{
-    int x, y;
+  ws2811_led_t leds[] = { 0xff0000, 0x00ff00, 0x0000ff };
 
-    for (x = 0; x < WIDTH; x++)
-    {
-        for (y = 0; y < HEIGHT; y++)
-        {
-            ledstring.channel[0].leds[(y * WIDTH) + x] = matrix[x][y];
-        }
-    }
-}
-
-void matrix_raise(void)
-{
-    int x, y;
-
-    for (y = 0; y < (HEIGHT - 1); y++)
-    {
-        for (x = 0; x < WIDTH; x++)
-        {
-            matrix[x][y] = matrix[x][y + 1];
-        }
-    }
-}
-
-int dotspos[] = { 0, 1, 2, 3, 4, 5, 6, 7 };
-ws2811_led_t dotcolors[] =
-{
-    0x200000,  // red
-    0x201000,  // orange
-    0x202000,  // yellow
-    0x002000,  // green
-    0x002020,  // lightblue
-    0x000020,  // blue
-    0x100010,  // purple
-    0x200010,  // pink
-};
-
-void matrix_bottom(void)
-{
+  while (1) {
     int i;
-
-    for (i = 0; i < ARRAY_SIZE(dotspos); i++)
-    {
-        dotspos[i]++;
-        if (dotspos[i] > (WIDTH - 1))
-        {
-            dotspos[i] = 0;
-        }
-
-        matrix[dotspos[i]][HEIGHT - 1] = dotcolors[i];
+    for (i = 0; i < count-3; i+=3) {
+      ledstring.channel[0].leds[i+0] = leds[0+(counter%3)];
+      ledstring.channel[0].leds[i+1] = leds[1+(counter%3)];
+      ledstring.channel[0].leds[i+2] = leds[2+(counter%3)];
     }
-}
+    counter++;
 
-static void ctrl_c_handler(int signum)
-{
-    ws2811_fini(&ledstring);
-}
-
-static void setup_handlers(void)
-{
-    struct sigaction sa =
-    {
-        .sa_handler = ctrl_c_handler,
-    };
-
-    sigaction(SIGKILL, &sa, NULL);
-}
-
-int main(int argc, char *argv[])
-{
-    int ret = 0;
-
-    setup_handlers();
-
-    if (ws2811_init(&ledstring))
-    {
-        return -1;
+    if (ws2811_render(&ledstring)) {
+      break;
     }
 
-    while (1)
-    {
-        matrix_raise();
-        matrix_bottom();
-        matrix_render();
+    usleep(1000000);
+  }
 
-        if (ws2811_render(&ledstring))
-        {
-            ret = -1;
-            break;
-        }
-
-        // 15 frames /sec
-        usleep(1000000 / 15);
-    }
-
-    ws2811_fini(&ledstring);
-
-    return ret;
+  ws2811_fini(&ledstring);
+  exit(EXIT_FAILURE);
 }
 
