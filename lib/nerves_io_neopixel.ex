4e82cc9099dab9090c1a7718896bdcbef2a5a538
Greg Mefford <greg@gregmefford.com>
2016-01-04 23:28:03 -0500

    Added a BSG-style Neopixel.scan function

--- lib/nerves_io_neopixel.ex
+++ lib/nerves_io_neopixel.ex
@@ -49,9 +49,28 @@ defmodule Nerves.IO.Neopixel do
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 
-  def render(pid, pixel_data) do
-    Logger.debug "#{__MODULE__} (pid: #{inspect pid}) rendering: #{inspect pixel_data}"
+  def render(_pid, pixel_data) do
+    Logger.debug "#{__MODULE__} (pid: #{inspect _pid}) rendering: #{inspect pixel_data}"
     GenServer.call(:nerves_io_neopixel, {:render, pixel_data})
+  end
+
+  def scan(pid, delay_ms, led_count, repetitions \\ 1)
+
+  def scan(_pid, _delay_ms, _led_count, 0), do: :ok
+
+  def scan(pid, delay_ms, led_count, repetitions) when led_count >= 0 and repetitions >= 0 do
+    frames = Stream.concat(0..(led_count-1), (led_count-1)..0)
+    Enum.each(frames, &_scan(&1, pid, delay_ms, led_count))
+    scan(pid, delay_ms, led_count, repetitions - 1)
+  end
+
+  defp _scan(index, pid, delay_ms, led_count) do
+    offset = rem(index, led_count)
+    front_buffer_bits = offset * 8 * 3
+    back_buffer_bits  = (led_count - offset - 1) * 8 * 3
+    pixel_data = << 0 :: size(front_buffer_bits), 255, 0, 0, 0 :: size(back_buffer_bits) >>
+    render(pid, pixel_data)
+    :timer.sleep(delay_ms)
   end
 
 end
