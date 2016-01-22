c5e42dae8f9a1e9c32776f88a14b302098291ab7
Greg Mefford <greg@gregmefford.com>
2016-01-21 22:09:58 -0500

    Split up the modules into their own files

--- lib/nerves/io/neopixel.ex
+++ lib/nerves/io/neopixel.ex
@@ -1,32 +1,5 @@
-defmodule Nerves.IO.Neopixel.Driver do
-
-  use GenServer
-  require Logger
-
-  def start_link(settings, opts) do
-    Logger.debug "#{__MODULE__} Starting"
-    GenServer.start_link(__MODULE__, settings, opts)
-  end
-
-  def init(settings) do
-    Logger.debug "#{__MODULE__} initializing: #{inspect settings}"
-    pin   = settings[:pin]
-    count = settings[:count]
-
-    cmd = "#{:code.priv_dir(:nerves_io_neopixel)}/rpi_ws281x #{pin} #{count}"
-    port = Port.open({:spawn, cmd}, [:binary])
-    {:ok, port}
-  end
-
-  def handle_call({:render, pixel_data}, _from, port) do
-    Logger.debug "#{__MODULE__} rendering: #{inspect pixel_data}"
-    Port.command(port, pixel_data)
-    {:reply, :ok, port}
-  end
-
-end
-
 defmodule Nerves.IO.Neopixel do
+  @moduledoc false
 
   use Application
   require Logger
--- test/neopixel_test.exs
+++ test/neopixel_test.exs
@@ -6,12 +6,12 @@ defmodule Nerves.IO.Neopixel.Test do
   alias Nerves.IO.Neopixel
 
   test "configuring a Neopixel interface" do
-    {:ok, pid} = Neopixel.setup pin: "18", count: "5"
+    {:ok, pid} = Neopixel.setup pin: 18, count: 5
     assert is_pid(pid)
   end
 
   test "rendering a pixel" do
-    {:ok, pid} = Neopixel.setup pin: "18", count: "5"
+    {:ok, pid} = Neopixel.setup pin: 18, count: 5
     assert is_pid(pid)
     assert Neopixel.render(pid, <<255, 0, 16>>) == :ok
   end
